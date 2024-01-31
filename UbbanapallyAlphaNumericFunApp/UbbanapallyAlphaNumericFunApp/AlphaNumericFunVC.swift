b51925fc3dc2c6e5e044e0c246c5312dc7d05042
AjitUbbanapally <ajitpurnahk@gmail.com>
2024-01-31 11:53:12 -0600

    Labels

---
+++ UbbanapallyAlphaNumericFunApp/UbbanapallyAlphaNumericFunApp/AlphaNumericFunVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UbbanapallyAlphaNumericFunApp
+//
+//  Created by ajit hk on 28/01/24.
+//
+
+import UIKit
+
+class AlphaNumericFunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
---
+++ iOS practice.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let ht = 40
+print(ht)
+let names = ["hari","Guru","Vaishnav"]
+for name in names[1...]
+{
+    print(name)
+}
+let range = ..<5
+range.contains(-1)
+range.contains(5)
+range.contains(4)
+
+
