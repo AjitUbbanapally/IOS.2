79a00ebc2799c2a8aa1ad38a4154561787e1d243
AjitUbbanapally <ajitpurnahk@gmail.com>
2024-04-03 20:45:47 -0500

    UI Design

---
+++ Spring24SampleExam02/RealEstateApp/PropertyDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  PropertyDetailVC.swift
+//  RealEstateApp
+//
+//  Created by ajit hk on 03/04/24.
+//
+
+import UIKit
+
+class PropertyDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
